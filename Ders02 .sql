a4037d8077d78d3c668da8da99a108c07ea3b781
Yusuf Sahin <yusuf@yusufsahin.net>
2024-05-02 21:49:24 +0300

    Update Ders2 SQL

--- Ders02 .sql
+++ Ders02 .sql
@@ -1,4 +1,4 @@
-SELECT * FROM Categories;
+﻿SELECT * FROM Categories;
 
 SELECT CategoryID, CategoryName FROM Categories;
 
@@ -25,3 +25,29 @@ SELECT * FROM Employees WHERE EmployeeID <5;
 SELECT * FROM Employees WHERE EmployeeID <=5;
 SELECT * FROM Employees WHERE EmployeeID >8;
 SELECT * FROM Employees WHERE EmployeeID >=8;
+
+INSERT INTO Categories (CategoryName,Description) VALUES ('Vegan','Vegan Foods'); 
+
+SELECT * FROM Categories; 
+
+UPDATE Categories SET CategoryName='Drinks' ,Description='Any Drinks' WHERE CategoryID= 10; 
+
+UPDATE Categories SET CategoryName='Drinks Update' ,Description='Any Drinks Update' WHERE CategoryID>=9; 
+
+
+SELECT * FROM Categories WHERE CategoryID>=9; 
+
+DELETE FROM Categories  WHERE CategoryID>=9; 
+
+DELETE FROM Categories;
+
+
+/*
+
+3 tane 
+Name ve Description -> CategoryX ve CategoryX Description eklencek
+
+son 2 kayıt CategoryName X-> CategoryXUpdate 
+
+Ve Son kayıt silinecek 
+*/
